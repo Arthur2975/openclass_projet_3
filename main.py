a51b7fed25a696f84633ce758d3ea561c04cadfe
Arthur Mengual <arthur.mengual@gmail.com>
2021-04-06 10:27:36 +0200

    integration des match dans le json

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from tinydb import TinyDB, Query
 import json
+from tinydb import TinyDB, Query
 from classes import player as play
 from classes import tournoi as tour
-
 
 db = TinyDB('chess.json')
 
@@ -69,57 +68,64 @@ def match_first_round():
     # enter scores:
     score_1 = input('Enter ' + upper_group[0].name + 's' + ' score')
     score_2 = input('Enter ' + lower_group[0].name + 's' + ' score')
-    first_match = (upper_group[0].name, score_1, lower_group[0].name, score_2)
+
     # second match
     print('second match : ' +
           upper_group[1].name + ' against ' + lower_group[1].name)
     # enter scores:
     score_3 = input('Enter ' + upper_group[1].name + 's' + ' score')
     score_4 = input('Enter ' + lower_group[1].name + 's' + ' score')
-    second_match = (upper_group[1].name, score_3, lower_group[1].name, score_4)
+
     # third match
     print('third_match : ' +
           upper_group[2].name + ' against ' + lower_group[2].name)
     # Enter scores:
     score_5 = input('Enter ' + upper_group[2].name + 's' + ' score')
     score_6 = input('Enter ' + lower_group[2].name + 's' + ' score')
-    third_match = (upper_group[2].name, score_5, lower_group[2].name, score_6)
+
     # fourth match
     print('fourth_match : ' +
           upper_group[3].name + ' against ' + lower_group[3].name)
     # Enter scores:
     score_7 = input('Enter ' + upper_group[3].name + 's' + ' score')
     score_8 = input('Enter ' + lower_group[3].name + 's' + ' score')
-    fourth_match = (upper_group[3].name, score_7, lower_group[3].name, score_8)
 
-    round_scores = [first_match, second_match, third_match, fourth_match]
+    serialized_match = {'first_match': str(upper_group[0].name) + ' against ' + str(lower_group[0].name),
+                        'second_match': str(upper_group[1].name) + ' against ' + str(lower_group[1].name),
+                        'third_match': str(upper_group[2].name) + ' against ' + str(lower_group[2].name),
+                        'fourth_match': str(upper_group[3].name) + ' against ' + str(lower_group[3].name)}
+    match_table = db.table('matchs')
+    match_table.insert(serialized_match)
 
-    # with open('scores.json', 'w') as f_score:
-    #     for item in round_scores:
-    #         f_score.write(item)  # deconne veut pas enregistrer des tuples
-
+    serialized_scores = {upper_group[0].name: score_1, lower_group[0].name: score_2, upper_group[1].name: score_3,
+                         lower_group[1].name: score_4, upper_group[2].name: score_5,
+                         lower_group[2].name: score_6, upper_group[3].name: score_7,
+                         lower_group[3].name: score_8}
     scores_table = db.table('scores')
-    scores_table.insert(round_scores)  # serializer
+    scores_table.insert(serialized_scores)
 
 
 def match_second_round():
     '''This function generates the second round's matchs. It generates pairs of players according to their first round's results, allow to enter scores
     and save it into the score file '''
+    table_scores = db.table('scores')
+    all_scores = table_scores.all()
+    # recuperer aussi les match pour pas qu'il y ait de doublons
 
-    pass
-    # trier les joueurs en fonction du nombre du points au premier tour
-    # si même nombre de points, alors utiliser ranking
-    # joueur 1 contre joueur 2, j3 contre j4 etc...
-    # si les joueurs se sont déjà rencontrés alors joueur 1 contre j3
+
+# trier les joueurs en fonction du nombre du points au premier tour
+# si même nombre de points, alors utiliser ranking
+# joueur 1 contre joueur 2, j3 contre j4 etc...
+# si les joueurs se sont déjà rencontrés alors joueur 1 contre j3
+
 
 # trier la liste de joueur par ordre de points au premier tour.
 # les gagnants ont 1 les perdants ont 0 donc si les 1 jouent entre eux et les zero entre eux il n'y a pas de doublons
 # sorted_scores = sorted(liste of scores, key=lambda ...)
 #import numpy as np
-# a = np.array(tuples??)
+
+if __name__ == '__main__':
+    main()
 
 
-# if __name__ == '__main__':
-#     main()
-
-match_first_round()
+# enregistrer une liste de match puis verifier si le match a deja eu lieu
